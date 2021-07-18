0af845ef6852908f3c5817027f13d1f0f8d79e97
Mukesh Kumar Chaurasiya <mukeshmike9i@gmail.com>
2021-07-18 14:21:22 +0530

    PCD implemented

--- custom_drivers/002.CharacterDriver/lkm.c
+++ custom_drivers/002.CharacterDriver/lkm.c
@@ -3,6 +3,7 @@
 #include <linux/cdev.h>
 #include <linux/types.h>
 #include <linux/device.h>
+#include <linux/uaccess.h>
 #undef pr_fmt
 #define pr_fmt(fmt) "%s : " fmt, __func__
 
@@ -17,20 +18,90 @@ dev_t dev_number;
 struct cdev pcd_cdev;
 loff_t pcd_lseek(struct file *filp, loff_t off, int whence)
 {
-    pr_info("Lseek Requested:\n");
-    return 0;
+    loff_t temp;
+    pr_info("Current file position:\n", filp->f_pos);
+    switch (whence)
+    {
+    case SEEK_SET:
+        if ((off > DEV_MEM_SIZE) || (off < 0))
+        {
+            return -EINVAL;
+        }
+        filp->f_pos = off;
+        break;
+    case SEEK_CUR:
+        temp = filp->f_pos + off;
+        if((temp > DEV_MEM_SIZE) || (temp < 0)) {
+            return -EINVAL;
+        }
+        filp->f_pos = temp;
+        break;
+    case SEEK_END:
+        temp = DEV_MEM_SIZE + off;
+        if((temp > DEV_MEM_SIZE) || (temp < 0)) {
+            return -EINVAL;
+        }
+        filp->f_pos = temp;
+        break;
+    default:
+        return -EINVAL;
+    }
+
+    pr_info("New file position:\n", filp->f_pos);
+    return filp->f_pos;
 }
 
-ssize_t pcd_read(struct file *filp, char __user *buff, size_t len, loff_t *offset)
+ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos)
 {
-    pr_info("Read Requested\n");
-    return 0;
+    pr_info("Read requested for %zu bytes\n", count);
+    pr_info("Current file position = %lld\n", *f_pos);
+    /* Adjust the count */
+    if ((*f_pos + count) > DEV_MEM_SIZE)
+    {
+        count = DEV_MEM_SIZE - *f_pos;
+    }
+
+    /* copy to user */
+    if (copy_to_user(buff, &pseudo_device[*f_pos], count))
+    {
+        return -EFAULT;
+    }
+
+    /*Update the curent offset*/
+    *f_pos += count;
+
+    pr_info("No of bytes read = %zu\n", count);
+    pr_info("New File position = %lld\n", *f_pos);
+    return count;
 }
 
-ssize_t pcd_write(struct file *filp, const char __user *buff, size_t len, loff_t *offset)
+ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
 {
-    pr_info("Write requested\n");
-    return 0;
+    pr_info("Write requested for %zu bytes\n", count);
+    pr_info("Current file position = %lld\n", *f_pos);
+    /* Adjust the count */
+    if ((*f_pos + count) > DEV_MEM_SIZE)
+    {
+        count = DEV_MEM_SIZE - *f_pos;
+    }
+
+    if (!count)
+    {
+        return -ENOMEM;
+    }
+
+    /* copy to user */
+    if (copy_from_user(&pseudo_device[*f_pos], buff, count))
+    {
+        return -EFAULT;
+    }
+
+    /*Update the curent offset*/
+    *f_pos += count;
+
+    pr_info("No of bytes wrote = %zu\n", count);
+    pr_info("New File position = %lld\n", *f_pos);
+    return count;
 }
 
 int pcd_open(struct inode *inode, struct file *filp)
@@ -61,8 +132,13 @@ struct device *pcd_device;
 
 static int __init pcd_init(void)
 {
+    int ret;
     /*1. Dynamically Allocate a device number */
-    alloc_chrdev_region(&dev_number, 0, 1, "pcd");
+    ret = alloc_chrdev_region(&dev_number, 0, 1, "pcd");
+    if(ret < 0) {
+        pr_err("alloc_chrdev_region Failed\n");
+        goto out;
+    }
     pr_info("[PCD Device Number]: <Major:Minor> = %d:%d\n", MAJOR(dev_number), MINOR(dev_number));
 
     /*2. initialize a device structure */
@@ -70,14 +146,37 @@ static int __init pcd_init(void)
 
     /*3. Register Cdev Structure with VFS */
     pcd_cdev.owner = THIS_MODULE;
-    cdev_add(&pcd_cdev, dev_number, 1);
-
+    ret = cdev_add(&pcd_cdev, dev_number, 1);
+    if(ret < 0) {
+        pr_err("cdev_add Failed\n");
+        goto unreg_chrdev;
+    }
     /*4. Create Device class under /sys/class/  */
     pcd_class = class_create(THIS_MODULE, "pcd_class");
+    if(IS_ERR(pcd_class)) {
+        pr_err("Class creation Failed\n");
+        ret = PTR_ERR(pcd_class);
+        goto del_dev;
+    }
 
     /*5. Populate the sysfs with device information */
     pcd_device = device_create(pcd_class, NULL, dev_number, NULL, "pcd");
+        if(IS_ERR(pcd_device)) {
+        pr_err("Device creation Failed\n");
+        ret = PTR_ERR(pcd_device);
+        goto del_class;
+    }
     return 0;
+
+del_class: 
+    class_destroy(pcd_class);
+del_dev:
+    cdev_del(&pcd_cdev);
+unreg_chrdev:
+    unregister_chrdev_region(dev_number, 1);
+out:
+    pr_err("Module Insertion Failed\n");
+    return ret;
 }
 
 static void __exit pcd_exit(void)
